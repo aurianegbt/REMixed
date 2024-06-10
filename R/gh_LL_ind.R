ca226fcc9aadc1b6e97360ab96fec33eef84a6ff
Auriane Gabaut <122619227+aurianegbt@users.noreply.github.com>
2024-06-10 11:57:15 +0200

    add data argument fixed error

--- R/gh_LL_ind.R
+++ R/gh_LL_ind.R
@@ -11,7 +11,7 @@
 #'
 #' @param mu_i Named vector of individual random effetcs estimation (size m);
 #' @param Omega_i Named diagonal matrix of individual standard deviation estimation (size m x m) ;
-#' @param theta model parameter ; contain phi_pop (size L), psi_pop (size m), gamma (list of size L, containing vector of size m), beta (list of size m, containing vector of size m), alpha0 (size K);
+#' @param theta model parameter ; contain phi_pop (size L), psi_pop (size m), gamma (list of size L, containing vector of size m), beta (list of size m, containing vector of size m), alpha0 (size K), and omega (size m) ;
 #' @param alpha1 Regularization parameter (size K ) ; must be in the same order as in Robs_i.
 #' @param dynFun Dynamic function ;
 #' @param y Initial condition of the model, conform to what is asked in dynFun ;
@@ -40,19 +40,91 @@
 #' @examples
 #' #TODO
 
-gh.LL.ind <- function(mu_i,Omega_i,theta,alpha1,dynFun,y,covariates_i,ParModel.transfo,ParModel.transfo.inv,Sobs_i,Robs_i,Serr,Rerr,ObsModel.transfo,n=floor(100**(1/length(theta$psi_pop))),prune=NULL,onlyLL=FALSE){
+gh.LL.ind <- function(
+    dynFun,
+    y,
+    mu_i=NULL,
+    Omega_i=NULL,
+    theta=NULL,
+    alpha1=NULL,
+    covariates_i=NULL,
+    ParModel.transfo=NULL,
+    ParModel.transfo.inv=NULL,
+    Sobs_i=NULL,
+    Robs_i=NULL,
+    Serr=NULL,
+    Rerr=NULL,
+    ObsModel.transfo=NULL,
+    data=NULL,
+    ind = NULL,
+    n = NULL,
+    prune=NULL,
+    onlyLL=FALSE){
   # check to do
 
-  if(ncol(Omega_i)!=1){
-    mh.parm <- mgauss.hermite(n,mu=mu_i,Omega=Omega_i,prune=prune)
+  if(is.null(data)){
+    test <- sapply(c("mu_i","Omega_i","theta","alpha1","covariates_i","ParModel.transfo","ParModel.transfo.inv","Sobs_i","Robs_i","Serr","Rerr","ObsModel.transfo"),FUN=is.null)
+    if(any(test))
+      stop("Please provide all necessary arguments.")
   }else{
-    mh.parm <- gauss.hermite(n,mu = mu_i,sd=sqrt(as.numeric(Omega_i)))
+    if((is.null(mu_i) || is.null(Omega_i)) & !all(c("mu","Omega") %in% names(data))){
+      stop("Please provide mu and Omega if these are missing from data.")
+    }
+    argsNONind = c("theta","alpha1","ParModel.transfo","ParModel.transfo.inv","Serr","Rerr","ObsModel.transfo")
+    for(d in 1:length(argsNONind)){
+      test <- eval(parse(text=paste0("is.null(",argsNONind[d],")")))
+      if(test){
+        eval(parse(text=paste0(argsNONind[d],"<- data[[argsNONind[d]]]")))
+      }
+    }
+    argsInd = c("mu","Omega","Robs","Sobs","covariates")
+    for(d in 1:length(argsInd)){
+      test <- eval(parse(text=paste0("is.null(",paste0(argsInd[d],"_i"),")")))
+      if(test){
+        eval(parse(text=paste0(argsInd[d],"<- data[[argsInd[d]]]")))
+
+      }
+    }
+    if((is.null(mu_i) || is.null(Omega_i) || is.null(Robs_i) || is.null(Sobs_i) || is.null(covariates_i)) & is.null(ind)){
+      stop("Please provide individual information (mu_i,Omega_i,Sobs_i,Robs_i,covariates_i), or the individual id ind.")
+    }
+    if(is.null(mu_i)){
+      mu_i = mu[[ind]]
+    }
+    if(is.null(Omega_i)){
+      Omega_i = Omega[[ind]]
+    }
+    if(is.null(Sobs_i)){
+      Sobs_i = lapply(Sobs,FUN=function(S){S[S$id==ind,]})
+    }
+    if(is.null(Robs_i)){
+      Robs_i = lapply(Robs,FUN=function(R){R[R$id==ind,]})
+    }
+    if(is.null(covariates_i)){
+      covariates_i = covariates[ind,,drop=F]
+    }
   }
+
+  if(is.null(n)){
+    n <- floor(100**(1/length(theta$psi_pop)))
+  }
+
+  dm = length(mu_i)
+
+  if(dm!=1){
+    mh.parm <- amgauss.hermite(n,mu=mu_i,Omega=Omega_i,prune=prune)
+    detsq.omega = prod(theta$omega)
+    root.omega = diag(1/theta$omega**2)
+  }else{
+    mh.parm <- agauss.hermite(n,mu = mu_i,sd=sqrt(as.numeric(Omega_i)))
+    detsq.omega = as.numeric(theta$omega)
+    root.omega = 1/as.numeric(theta$omega)**2
+  }
+
   nd = length(mh.parm$Weights)
-  N = nrow(covariates_i)
+  # N = nrow(covariates_i)
   R.sz = length(Rerr)
   S.sz = length(Serr)
-  root.Omega = solve(Omega_i)
   all.tobs = sort(union(unlist(lapply(Robs_i,FUN=function(x){x$time})),unlist(lapply(Sobs_i,FUN=function(x){x$time}))))
 
   # Need to compute, for each eta_i x individual i, the dynamics of the model
@@ -66,6 +138,7 @@ gh.LL.ind <- function(mu_i,Omega_i,theta,alpha1,dynFun,y,covariates_i,ParModel.t
   # Compute marginal latent density for each individual and value of RE
   R.margDensity <- setNames(sapply(1:nd,FUN=function(ei){
       dyn.ei = dyn[[paste0("eta_",ei)]]
+
 
       res = prod(sapply(1:R.sz,FUN=function(k){
         sig = Rerr[[k]]
@@ -84,6 +157,7 @@ gh.LL.ind <- function(mu_i,Omega_i,theta,alpha1,dynFun,y,covariates_i,ParModel.t
   # Compute marginal density for each individual and value of RE
   if(S.sz!=0){
     S.margDensity <- setNames(sapply(1:nd,FUN=function(ei){
+      eta_i = split(mh.parm$Points,1:nd)[[ei]]
       dyn.ei = dyn[[paste0("eta_",ei)]]
 
       res = prod(sapply(1:S.sz,FUN=function(p){
@@ -95,16 +169,18 @@ gh.LL.ind <- function(mu_i,Omega_i,theta,alpha1,dynFun,y,covariates_i,ParModel.t
         Spi = trs[[1]](dyn.ei[dyn.ei[,"time"] %in% tpi,names(trs)])
 
         prod(1/(sig*sqrt(2*pi))*exp(-1/2*((Ypi-Spi)/sig)**2))
-      }))
+      }))*(1/((2*pi)**(dm/2)*detsq.omega)*exp(-1/2*eta_i%*%root.omega%*%eta_i))
     }),paste0("eta_",1:nd))
   }else{
-    S.margDensity = setNames(rep(1,nd),paste0("eta_",1:nd))
+    S.margDensity = setNames(sapply(split(mh.parm$Points,1:nd),FUN=function(eta_i){
+      (1/((2*pi)**(dm/2)*detsq.omega)*exp(-1/2*eta_i%*%root.omega%*%eta_i))
+    }),paste0("eta_",1:nd))
   }
-
 
   # Compute individual log-Likelihood
   Li = sum(mh.parm$Weights*R.margDensity*S.margDensity)
   LLi = log(Li)
+
 
   if(!onlyLL){
     # Compute  gradient of individual log-Likelihood
@@ -121,7 +197,7 @@ gh.LL.ind <- function(mu_i,Omega_i,theta,alpha1,dynFun,y,covariates_i,ParModel.t
         trs = ObsModel.transfo[[2]][k]
         Rki = trs[[1]](dyn.ei[dyn.ei[,"time"] %in% tki,names(trs)])
 
-        return(1/(sig**2)*(sum(alpha1[[k]]*Rki*(Zki-theta$alpha0[[k]]-alpha1[[k]]*Rki))))
+        return(1/(sig**2)*(sum(Rki*(Zki-a0-a1*Rki))))
       }),paste0("eta_",1:nd))
     })
 
@@ -145,15 +221,14 @@ gh.LL.ind <- function(mu_i,Omega_i,theta,alpha1,dynFun,y,covariates_i,ParModel.t
         Rki = trs[[1]](dyn.ei[dyn.ei[,"time"] %in% tki,names(trs)])
 
         return(
-          (1/sig**4*(sum(alpha1[[k]]*Rki*(Zki-theta$alpha0[[k]]-alpha1[[k]]*Rki)))**2 +
-             1/sig**2*(sum(Rki*(Zki-theta$alpha0[[k]]-2*alpha1[[k]]*Rki))))
-        )
+          (1/(sig**2)*(sum(Rki*(Zki-a0-a1*Rki))))**2 - 1/(sig**2)*(sum(Rki**2))
+          )
       }),paste0("eta_",1:nd))
     })
 
     diag(ddLLi) = sapply(ddfact,FUN=function(f){
       return(1/Li*sum(mh.parm$Weights*f*R.margDensity*S.margDensity))
-    })-dLLi**2
+    })-(dLLi)**2
 
     # non diagonal term
     for(k1 in 1:R.sz){
@@ -173,7 +248,7 @@ gh.LL.ind <- function(mu_i,Omega_i,theta,alpha1,dynFun,y,covariates_i,ParModel.t
               Rki = trs[[1]](dyn.ei[dyn.ei[,"time"] %in% tki,names(trs)])
 
               return(
-                1/sig**2*(sum(alpha1[[k]]*Rki*(Zki-theta$alpha0[[k]]-alpha1[[k]]*Rki)))**2
+                1/sig**2*(sum(Rki*(Zki-a0-a1*Rki)))
               )
             }))
           }),paste0("eta_",1:nd))
@@ -189,63 +264,26 @@ gh.LL.ind <- function(mu_i,Omega_i,theta,alpha1,dynFun,y,covariates_i,ParModel.t
 }
 
 
-# Hermite polynomial ------------------------------------------------------
-# \int f(x)exp(-x²)dx=sum wi * f(xi)
-hermite <- function(n){
-  # Hn+1 = XHn - nHn-1
-  # degré max n
-  # n>=1
-  H0 <- c(1,rep(0,n))
-  H1 <- c(0,2,rep(0,n-1))
-  if(n>1){
-    for(k in 1:(n-1)){
-      H0.prev = H0
-      H0 <- H1
-      H1 <- 2*(data.table::shift(H1,fill = 0) - k*H0.prev)
-      # print(H1)
-    }
-  }
-  # H0 c'est notre Hn-1 et H1 notre H1
-
-  ## Polynôme de Gauss
-  x <- sort(Re(polyroot(H1))) # calcul des xi comme racine du polynôme d'hermite Hn(x)
-  H0x <- sapply(x,FUN=function(xi){
-    res <- 0
-    for(k in 0:(n-1)){
-      res <- res + H0[k+1]*xi**k
-    }
-    return(res)
-  }) # calcul du polynôme Hn-1 en chaque points (xi)
-  w <- 2**(n + 1) * factorial(n) * sqrt(pi)/(2*n*H0x)^2 # calcul des poids wi= 2**(n+1)*factorial(n)*sqrt(pi) / H'n(xi)**2 et H'n(x)=2nHn-1(x)
-
-  return(data.frame(Points=x,Weights=w))
-}
-# tester pour n=10 ; f=id ; f=x ; f=x^2 et à l'air good
-
-
-# One dimensional hermite gauss  ------------------------------------------
-# changement de variable pour int f(eta) deta selon mesure normale i.e int f(x) 1/(sd**2 sqrt(2*pi))*exp(-1/2((x-mu)/sd)**2)dx = sum(wi f(xi))
-gauss.hermite <- function(n,mu=0,sd=1){
-  gh <- hermite(n)
+# One dimensional Adaptative Hermite Gauss  ------------------------------------------
+agauss.hermite <- function(n,mu=0,sd=1){
+  gh <- fastGHQuad::gaussHermiteData(n)
+  names(gh) <- c("Points","Weights")
 
   ## Transformation pour N(mu,sigma)  (sigma=sd)
-  ww <- gh$Weights/sqrt(pi)
+  ww <- gh$Weights * sqrt(2) * sd * exp(gh$Points**2)
   xx <- mu + sd * sqrt(2) * gh$Points
 
   return(data.frame(Points=xx,Weights=ww))
 }
-# tester pour n=10 ; f=id ; f=x ; f=x^2 et à l'air good
 
 
-# Multi-dimensional Hermite Gauss -----------------------------------------
-# ref http://www.jaeckel.org/ANoteOnMultivariateGaussHermiteQuadrature.pdf
-# The notion of ‘pruning’ can be used to eliminate those points with very small weights.
-mgauss.hermite <- function(n,mu=rep(0,ncol(Omega)),Omega=diag(rep(1,length(mu))),prune=NULL){
+# Multi-dimensional Adaptative Hermite Gauss -----------------------------------------
+amgauss.hermite <- function(n,mu=rep(0,ncol(Omega)),Omega=diag(rep(1,length(mu))),prune=NULL){
   # L'idée est on prends les points en dim 1 et on les rotatent/translatent
   dm = length(mu)
 
-  gh  <- gauss.hermite(n)
-  # plot(idx)  dm  <- length(mu)
+  gh  <- as.data.frame(fastGHQuad::gaussHermiteData(n))
+
   idx <- as.matrix(expand.grid(rep(list(1:n),dm)))
   pts <- matrix(gh[idx,1],nrow(idx),dm)
   wts <- apply(matrix(gh[idx,2],nrow(idx),dm), 1, prod)
@@ -259,21 +297,13 @@ mgauss.hermite <- function(n,mu=rep(0,ncol(Omega)),Omega=diag(rep(1,length(mu)))
   }
 
   eig <- eigen(Omega)
-  rot <- eig$vectors %*% diag(sqrt(eig$values))
-  pts <- t(rot %*% t(pts) + mu)
-  # colnames(pts) <-c("X","Y")
-  # ggplot(mapping=aes(x=X,y=Y))+geom_density_2d(data=data,aes(color = after_stat(level)))+scale_color_viridis_c()+geom_point(data=as.data.frame(pts,wts=wts),aes(size=-5/log(wts)))
-  return(list(Points=pts, Weights=wts))
+  S = eig$vectors
+  L = diag(sqrt(eig$values))
+  rot <- S %*% L
+
+  ppts  <-t( sqrt(2) * rot %*% t(pts) + mu)
+  wwts  <- wts * sqrt(2)**dm * det(rot) * exp(apply(pts,1,FUN=function(x){t(x)%*%x}))
+
+
+  return(list(Points=ppts, Weights=wwts))
 }
-# j'ai bien la densité d'une loi normale vaut 1
-# j'arrive à retrouver P(X<0,Y<0) pour loi normale centrée réduite
-# idem en plus grande dimension n=6 au lieu de 2
-# > Omega=diag(rep(1,6))
-# > n
-# [1] 2
-# > mu=rep(0,6)
-# > t <- mgauss.hermite(n,mu,Omega)
-# > sum(t$Weights*apply(t$points,1,FUN=gfun))
-# [1] 0.015625
-# > 1/(2**6)
-# [1] 0.015625
