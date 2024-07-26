3d61c365b2ab42d62709cb765117afd6c6d79e3f
Auriane Gabaut <122619227+aurianegbt@users.noreply.github.com>
2024-07-26 09:46:11 +0200

    Add linkS and linkR

--- R/readMLX.R
+++ R/readMLX.R
@@ -1,22 +1,28 @@
-#' Gather information from a monolix project
+#' Gather information from a monolix project for REMix algorithm.
 #'
-#' Retrieve all information necessary for the algorithm that can be search in the created monolix project.
+#' Retrieve all information necessary for the algorithm that can be searched in the created monolix project.
 #'
-#' @param project directory of the monolix project (in .mlxtran), if NULL get current project load, default to NULL
-#' @param ObsModel.transfo list of 2 list of P,K transformation (need to include identity transformation), named with `S` and `R` :
+#' @param project directory of the monolix project (in .mlxtran), if NULL get current project load (default to NULL);
+#' @param ObsModel.transfo list of 2 list of P,K transformation (it needs to include identity transformation), named with `S` and `R`, and two vector with link to the observation model of the monolix project named with `linkS` and `linkR`. Both `ObsModel.transfo$S` (resp. `ObsModel.transfo$R`) and `ObsModel.transfo$linkS` (resp. `ObsModel.transfo$linkR`) need to have the same length. (see \code{\link{lixoftConnectors::getContinuousObservationModel}} or \code{\link{lixoftConnectors::getObservationInformation}})
+#'   - `ObsModel.transfo$S` corresponds to the transformation used for direct observation model. For each \eqn{Y_p=h_p(S_p)}, the name indicate which dynamic from monolix is observed through this variables \eqn{Y_p}. The link vector `ObsModel.transfo$linkS` indicate for each transformation, mention in the same order as in `ObsModel.transfo$S`, what is the observation model name corresponding in monolix.
+#'   - `ObsModel.transfo$R` corresponds to the transformation used for the latent process, as it is now, we only have one latent dynamic so necessarily. \eqn{s_k} is applied to the same dynamic but for each \eqn{Y_k} observed, transformation could be different so need to precise as many as in the monolix project created, and the name should be the output from `dynFUN` crresponding. The link vector `ObsModel.transfo$linkR` indicate for each transformation, mention in the same order as in `ObsModel.transfo$R`, what is the observation model name corresponding in monolix.
+#' @param alpha list of named vector "alpha0", "alpha1". `alpha$alpha1` is mandatory even if equal to 1. The name of `alpha$alpha0` and `alpha$alpha1` are the observation model names from the monolix project to which they are linked. (if alpha_0 vector empty then all alpha_0 set to 0 ; # if some alpha_0 not define but not all, set NULL to the one missing).
 #'
-#'   - ObsModel.transfo$S correspond to the transformation used for direct observation model. For each \eqn{Y_p=h_p(S_p)}, the name indicate which dynamic from monolix is observed through this variables \eqn{Y_p};
-#'
-#'   - ObsModel.transfo$R correspond to the transformation used for the latent process, as it is now, we only have one latent dynamic so necessarily \eqn{s_k} is applied to `R` but for each \eqn{Y_k} observed, transformation could be different so need to precise as many as in the monolix project created ;
-#' @param alpha list of named vector "alpha0", "alpha1" (in good order), alpha1 mandatory even if 1
-#'
-# if alpha_0 vector empty -> all alpha_0 set to 0 ;
-# if some alpha_0 not define but not all, set NULL to the one missing
-#'
-#' @return list with parameters, transformations, observations from the monolix project, with the form needed for the implemented algorithm
+#' @return list with parameters, transformations, observations from the monolix project, with the form needed for the implemented algorithm. If a SAEM hasn't been launched, its the initial condition that are returned.
+#'  \itemize{\item `mu` list of estimated mean of the individual random effects distribution a posteriori (if conditional distribution estimation through monolix has been launched for the project).  ;
+#'  \item `Omega` list of the estimated variance-covariance matrix of the  individual random effects distribution a posteriori (if conditional distribution estimation through monolix has been launched for the project) ;
+#'  \item `theta` list of `phi_pop`, `psi_pop` population parameters (whithout and whith r.e. respectively); `gamma`and `beta` the estimated covariates effects coefficients for each population parameters vector; `omega` the estimated variance of r.e. for `psi_pop` parameters.
+#'  \item `alpha1` named vector with the values of regression parameters \eqn{(\alpha_1)_{k\leq K}} linked to our latent process. Names are the observation model names from the monolix project.
+#'  \item `covariates`data.frame with covariates (col.) for each individual (row).
+#'  \item `ParModel.transfo`and `ParModel.transfo.inv` named list of transformation function for individual parameter model.
+#'  \item `Sobs` list (size N) of list of direct observation (size P), each element contain time and observation.
+#' \item `Robs` list (size N) of latent observation (size K), each element contain time and observation.
+#' \item `Serr` vector of size P containing estimated error model constant.
+#' \item `Rerr` vector of size K containing estimated error model constant.
+#' \item `ObsModel.transfo` given in input.}
 #' @export
 #'
-#' @seealso \code{\link{Remix}}, \code{\link{cv.Remix}}.
+#' @seealso \code{\link{Remix}}, \code{\link{cv.Remix}}, \code{\link{lixoftConnectors::getContinuousObservationModel}},  \code{\link{lixoftConnectors::getObservationInformation}}
 #'
 #' @examples
 #' # TODO
@@ -28,6 +34,15 @@ readMLX <- function(project=NULL,
 
   # check length alpha1 equal to length alpha0 if not null equal to length obs$R
   # check obs output name equal to the one in ObsModel.transfo
+  if(length(ObsModel.transfo$S)!=length(ObsModel.transfo$linkS) || length(ObsModel.transfo$R)!=length(ObsModel.transfo$linkR)){
+    stop("Error in length of ObsModel.transfo arguments. S and linkS (resp. R and linkR) should have the same length.")
+  }
+  if(length(alpha$alpha1)!=length(ObsModel.transfo$R)){
+    stop("Error in length of alpha1 parameters. alpha$alpha1 and ObsModel.transfo$R should have the same size. ")
+  }
+  if(!all(names(alpha$alpha1)==ObsModel.transfo$linkR) || (!is.null(alpha$alpha0) && !all(names(alpha$alpha0)==ObsModel.transfo$linkR))){
+    stop("Error in names of ObsModel.transfo and alpha. They must be consistent (and in the same order).")
+  }
 
   suppressMessages({
     if (!is.null(project)){
@@ -117,25 +132,26 @@ readMLX <- function(project=NULL,
     }),psi.names)
   }else{NULL}
 
-  alpha0 <- rep(0,length(ObsModel.transfo$R))
+  alpha0 <- setNames(rep(0,length(ObsModel.transfo$R)),ObsModel.transfo$linkR)
   if(!is.null(alpha$alpha0)){
-    alpha0[!is.null(alpha$alpha0)] <- value.params[value.params$name %in% paste0(alpha$alpha0,"_pop"),"initialValue"]
+    alpha0[!is.null(alpha$alpha0)] <- setNames(value.params[value.params$name %in% paste0(alpha$alpha0,"_pop"),"initialValue"],names(alpha$alpha0))
   }
-  alpha1 <- sapply(alpha$alpha1,FUN=function(a){
-    value.params[value.params$name==paste0(a,"_pop"),"initialValue"]},USE.NAMES = F)
+  alpha1 <- setNames(sapply(alpha$alpha1,FUN=function(a){
+    value.params[value.params$name==paste0(a,"_pop"),"initialValue"]},USE.NAMES = F),names(alpha$alpha1))
 
   theta = setNames(list(phi_pop,psi_pop,gamma,beta,alpha0,omega),c("phi_pop","psi_pop","gamma","beta","alpha0","omega"))
 
   # Serr, Rerr
   ErrorModel <- lixoftConnectors::getContinuousObservationModel()$parameters
-  names(ErrorModel) <- stringr::str_remove(names(ErrorModel),"y")
 
-  Serr <- sapply(names(ObsModel.transfo$S),FUN=function(x){
+  # ObsModel.transfo need to be name after ??? model name in monolix ??? or dyn ??? # need to look at that closer
+
+  Serr <- sapply(ObsModel.transfo$linkS,FUN=function(x){
     value.params[value.params$name==ErrorModel[[x]],"initialValue"]
-  },USE.NAMES = F)
-  Rerr <- sapply(setdiff(names(ErrorModel),names(ObsModel.transfo$S)),FUN=function(x){
+  },USE.NAMES = T)
+  Rerr <- sapply(ObsModel.transfo$linkR,FUN=function(x){
     value.params[value.params$name==ErrorModel[[x]],"initialValue"]
-  },USE.NAMES = F)
+  },USE.NAMES = T)
 
   # ParModel.transfo and inv
   ParModel <- IndividualParameterModel$distribution[union(phi.names,psi.names)]
@@ -156,12 +172,16 @@ readMLX <- function(project=NULL,
   }
   names(ParModel.transfo) <-  names(ParModel.transfo.inv) <- names(ParModel)
 
-  # Sobs Robs
-  Observation <- lixoftConnectors::getObservationInformation()[paste0("y",names(ErrorModel))]
-  names(Observation) <- stringr::str_remove(names(Observation),"y")
 
-  Sobs <-lapply(names(ObsModel.transfo$S),FUN=function(x){Observation[[x]]})
-  Robs <- lapply(setdiff(names(Observation),names(ObsModel.transfo$S)),FUN=function(x){Observation[[x]]})
+
+
+  browser()
+
+  # Sobs Robs
+  Observation <- lixoftConnectors::getObservationInformation()
+
+  Sobs <-lapply(ObsModel.transfo$linkS,FUN=function(x){Observation[[x]]})
+  Robs <- lapply(ObsModel.transfo$linkR,FUN=function(x){Observation[[x]]})
 
   if(!lixoftConnectors::getLaunchedTasks()$conditionalDistributionSampling){
     return(list(
@@ -216,6 +236,8 @@ readMLX <- function(project=NULL,
   }
 }
 
+
+# OK function -------------------------------------------------------------
 ok.beta <- function(formula,CovariateModel){
   form <- strsplit(formula, "\n")[[1]]
   for(p in 1:length(form)){
